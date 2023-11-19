303e3595d3219cdb767d44371fc4cc4bfecba7de
zijiandongkurt <57815948+zijiandongkurt@users.noreply.github.com>
2023-11-19 20:36:00 +0100

    refined a bit structure

--- serverSetup.py
+++ serverSetup.py
@@ -1,53 +1,69 @@
 import json
+import secrets
+
 
 def read_config(file_path):
     with open(file_path, 'r') as file:
         config_data = json.load(file)
     return config_data
 
-class client:
+
+class Client:
     def __init__(self):
         pass
-    def init_count():
-        #initialize the count
+
+    def init_count(self):
+        # initialize the count
         pass
 
-class user:
-    def __init__(self, config):
-        self.id = config["id"]
-        self.password = config["password"]
-        self.serverIp = config["server_ip"]
-        self.port = config["port"]
 
-    def increase():
-        #TODO: implement increase action
+class User:
+    def __init__(self, user_id, password, server_ip, port):
+        self.id = user_id
+        self.password = password
+        self.server_ip = server_ip if 'server_ip' in server_ip else "default_ip"
+        self.port = port if 'port' in port else "default_port"
+
+    def increase(self):
+        # TODO: implement increase action
         pass
-    def decrease():
-        #TODO: implement increase action
+
+    def decrease(self):
+        # TODO: implement decrease action
         pass
+
+
+# an example method for generating random id, password.
+def generate_credentials():
+    # Generate unique ID and password using secrets module
+    user_id = secrets.token_hex(8)
+    password = secrets.token_hex(12)
+    return user_id, password
+
 
 def registration():
-    #TODO: get ip and port(socket? idk)
+    # TODO: get ip and port (socket? idk)
 
-    #TODO: generate id
-    #TODO: generate password
-    id = 0
-    password = 0
-    return id,password
-    #if there's an error, then return false only
+    # Generate credentials
+    user_id, password = generate_credentials()
 
-back = registration
+    # TODO: handle errors during registration
 
-if(not back):
-    print("Here is an error")
+    return user_id, password
+
+
+# Perform registration
+user_credentials = registration()
+
+if not user_credentials:
+    print("There is an error during registration")
 else:
-    print("Your id is "+back[0])
-    print("Your password is "+back[1])
-    user = back
+    print("Your id is " + user_credentials[0])
+    print("Your password is " + user_credentials[1])
 
-
-
+# Read config file
 config_data = read_config('userInfos/config.json')
-user = user(config_data)
 
-
+# Create a User instance
+user_instance = User(*user_credentials, config_data.get("server_ip", "default_ip"),
+                     config_data.get("port", "default_port"))
