b2b8e89413ec412c05a97e7620a3778288547569
Garrus <91462257+Mikouer@users.noreply.github.com>
2023-12-29 16:32:19 +0100

    16：32 ZC

--- client.py
+++ client.py
@@ -3,6 +3,12 @@ import secrets
 import socket
 import time
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
 
 class User:
     @classmethod
@@ -19,12 +25,56 @@ class User:
         self.server_port = int(config_data["server"]["port"])
         self.actions = config_data["actions"]["steps"]
         self.delay = float(config_data["actions"]["delay"])
+        
 
     def increase(self, amount):
         print(f"User {self.id}: Increased counter by {amount}")
 
     def decrease(self, amount):
         print(f"User {self.id}: Decreased counter by {amount}")
+
+    def generateKeys(self):
+        # Generate an RSA Key
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=3072,
+            backend=default_backend()
+        )
+
+        # Get the Public Key from the Private Key
+        public_key = private_key.public_key()
+        self.public_key = public_key
+        self.private_key = private_key
+        
+    def encrypt(message,public_key):
+        # Message to be encrypted
+        message = message.encode('utf-8')
+
+        # Encrypting the message
+        encrypted = public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return encrypted
+
+    def decrypt(encrypted,private_key):
+        # Decrypting the message
+        original_message = private_key.decrypt(
+            encrypted,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        # Decode from bytes to string
+        decoded_message = original_message.decode('utf-8')
+        return decoded_message
 
     def connect_to_server(self):
         try:
